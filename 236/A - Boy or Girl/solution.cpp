284c58a2393c118747c2102e56d58c5d3cd57587
Avinash Kumar <avinashrk2107@gmail.com>
2026-09-13 13:36:52 +0530

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string userName;
+    cin>>userName;
+    int count = 0;
+    for(int i = 0; i < userName.length(); i++){
+        bool found = false;
+        for(int j = 0; j < i; j++){
+            if(userName[i] == userName[j] ){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            count++;
+        }
+    }
+    if(count % 2 == 0){
+        cout<<"CHAT WITH HER!";
+    }else{
+        cout<<"IGNORE HIM!";
+    }
+ 
+}
